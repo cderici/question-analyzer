97877f468fa714a1b8138d63e52d541e9dbf8424
keremcelik <kerem_celik@hotmail.com>
2013-11-24 20:10:35 +0200

    Commit
    Some comments added and QuestionAnalysis class implementation is
    started.

--- question_analysis.py
+++ question_analysis.py
@@ -1,18 +1,70 @@
 
 import codecs;
-from itertools import groupby;
 
 qFilePath = 'q.q';
 qParsedFilePath = 'q_parsed.qp';
 
+class Parser:
+
+    questions = [];
+
+    def getRawQuestionTexts(self, qFilePath):
+        qFile = codecs.open(qFilePath, 'r', 'utf-8');##utf-8 file
+        qTexts = qFile.readlines();
+        qTexts = [text.strip() for text in qTexts];
+
+        return qTexts; ## list of raw questions
+
+    def getParsedQuestionTexts(self, qParsedFilePath):
+        qFile = codecs.open(qParsedFilePath, 'r', 'utf-8');
+        qTexts = qFile.readlines();
+        qTexts = [text.strip().split('\t') for text in qTexts];
+
+        questions = [];
+        qParts = [];
+
+        for text in qTexts:
+            if(len(text) > 1):
+                if(text[1] == "."):
+                    
+                    qParts.append(text);
+                    questions.append(qParts);
+                    qParts = [];
+                else:
+                    qParts.append([t.replace('\ufeff', '') for t in text]);
+        
+        return questions;##Question parts -> list of list
+    
+    def parseFile(self, qFilePath, qParsedFilePath):
+        self.questions = [];
+
+        qTexts = self.getRawQuestionTexts(qFilePath);
+        qTextParts = self.getParsedQuestionTexts(qParsedFilePath);
+
+        length = len(qTexts);
+
+        for i in range(0, length):
+            self.questions.append(Question(qTexts[i], qTextParts[i]))            
+
+        return self.questions;
+
 class Question:
+    ##Raw question text
     questionText = '';
 
+    ##Dependency parsed question parts
     questionParts = [];
 
+    ##Root of the parts -- Always (.) period
     root = None;
 
-    def __init__(self, qText, qParts):
+    ##Focus of the question
+    focus = '';
+
+    ##LAT of the question
+    lat = '';
+
+    def __init__(self, qText= '', qParts = []):
         self.questionText = qText;
         self.questionParts = qParts;
 
@@ -37,49 +89,12 @@ class Question:
         else:
             return None;
 
-class Parser:
-
+class QuestionAnalysis:
     questions = [];
 
-    def getRawQuestionTexts(self, qFilePath):
-        qFile = codecs.open(qFilePath, 'r', 'utf-8');
-        qTexts = qFile.readlines();
-        qTexts = [text.strip() for text in qTexts];
-
-        return qTexts;
-
-    def getParsedQuestionTexts(self, qParsedFilePath):
-        qFile = codecs.open(qParsedFilePath, 'r', 'utf-8');
-        qTexts = qFile.readlines();
-        qTexts = [text.strip().split('\t') for text in qTexts];
-
-        questions = [];
-        qParts = [];
-
-        for text in qTexts:
-            if(len(text) > 1):
-                if(text[1] == "."):
-                    
-                    qParts.append(text);
-                    questions.append(qParts);
-                    qParts = [];
-                else:
-                    qParts.append([t.replace('\ufeff', '') for t in text]);
+    def __init__(self, questions = []):
+        self.questions = questions;
         
-        return questions;
-    
-    def parseFile(self, qFilePath, qParsedFilePath):
-        self.questions = [];
-
-        qTexts = self.getRawQuestionTexts(qFilePath);
-        qTextParts = self.getParsedQuestionTexts(qParsedFilePath);
-
-        length = len(qTexts);
-
-        for i in range(0, length):
-            self.questions.append(Question(qTexts[i], qTextParts[i]))            
-
-        return self.questions;
 
 parser = Parser();
 questions = parser.parseFile(qFilePath, qParsedFilePath);
