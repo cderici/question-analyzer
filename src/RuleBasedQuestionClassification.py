08192a30661e14c52d9a636c86ba5e75392cf08e
keremcelik <kerem_celik@hotmail.com>
2013-12-14 14:00:41 +0200

    Rule based Classification Changes

--- src/RuleBasedQuestionClassification.py
+++ src/RuleBasedQuestionClassification.py
@@ -1,5 +1,6 @@
 
 import codecs;
+import math;
 
 qFilePath = 'q.q';
 qParsedFilePath = 'q_parsed.qp';
@@ -123,7 +124,9 @@ class RuleClass:
     fineCategory = '';
 
     qxnWords = [];
+    qnxWordsScores = [];
     keywords = [];
+    keywordsScores = [];
 
     def __init__(self, coarseCategory, fineCategory):
         self.coarseCategory = coarseCategory;
@@ -131,12 +134,34 @@ class RuleClass:
 
         self.qxnWords = [];
         self.keywords = [];
+        self.qnxWordsScores = [];
+        self.keywordsScores = [];
 
     def addToQxnWords(self, word):
         self.qxnWords.append(word);
 
     def addToKeywords(self, word):
         self.keywords.append(word);
+
+    def getQxnWordScore(self, word, method):
+        for i in range(0, len(self.qxnWords)):
+            if(self.qxnWords[i] == word and method == 'e'):
+                return self.qnxWordsScores[i];
+            if(self.qxnWords[i].startswith(word) and method == 'c1'):
+                return self.qnxWordsScores[i];
+            if(word.startswith(self.qxnWords[i]) and method == 'c2'):
+                return self.qnxWordsScores[i];
+        return 0.0;
+
+    def getKeywordScore(self, word, method):
+        for i in range(0, len(self.keywords)):
+            if(self.keywords[i] == word and method == 'e'):
+                return self.keywordsScores[i];
+            if(self.keywords[i].startswith(word) and method == 'c1'):
+                return self.keywordsScores[i];
+            if(word.startswith(self.keywords[i]) and method == 'c2'):
+                return self.keywordsScores[i];
+        return 0.0;
 
 class RuleBasedQuestionClassification:
     questions = [];
@@ -149,6 +174,7 @@ class RuleBasedQuestionClassification:
         self.questions = questions;
 
         self.readClassDefinitions(classPath, classQuestionWordsPath, classQuestionKeywordsPath);
+        self.calculateFeatureScores();
 
     def getRuleClass(self, coarseCategory, fineCategory):
         results = [ruleClass for ruleClass in self.ruleClasses if ruleClass.coarseCategory == coarseCategory and ruleClass.fineCategory == fineCategory];
@@ -156,7 +182,28 @@ class RuleBasedQuestionClassification:
             return results[0];
         else:
             return None;
-    
+
+    def calculateFeatureScores(self):
+        allQnxWords = [];
+        [[allQnxWords.append(word) for word in ruleClass.qxnWords] for ruleClass in self.ruleClasses];
+
+        allKeywords = [];
+        [[allKeywords.append(word) for word in ruleClass.keywords] for ruleClass in self.ruleClasses];
+
+        for ruleClass in self.ruleClasses:
+            ruleClass.qnxWordsScores = [math.log(len(allQnxWords) / len([w for w in allQnxWords if w == word])) for word in ruleClass.qxnWords];
+            ruleClass.keywordsScores = [math.log(len(allKeywords) / len([w for w in allKeywords if w == word])) for word in ruleClass.keywords];
+
+        '''
+        for ruleClass in self.ruleClasses:
+            for i in range(0, len(ruleClass.qxnWords)):
+                print(ruleClass.qxnWords[i] + '\t' + str(ruleClass.qnxWordsScores[i]));
+        
+        for ruleClass in self.ruleClasses:
+            for i in range(0, len(ruleClass.keywords)):
+                print(ruleClass.keywords[i] + '\t' + str(ruleClass.keywordsScores[i]));
+        '''
+        
     def readClassDefinitions(self, classPath, classQuestionWordsPath, classQuestionKeywordsPath):
 
         ##Coarse and Fine Category
@@ -189,31 +236,18 @@ class RuleBasedQuestionClassification:
         
         file.close();
 
-    def exactMatch(self, l, i):
-        return i in l;
-
-    def containMatch1(self, l, i):
-        if(len([True for item in l if item.startswith(i)])):
-            return True;
-        return False;
-
-    def containMatch2(self, l, i):
-        if(len([True for item in l if i.startswith(item)])):
-            return True;
-        return False;
-
     def findCategories(self):
 
         self.finalCategory = [];
         
         for question in self.questions:
-            scores1 = [len([1 for item in question.questionParts if self.exactMatch(ruleClass.qxnWords, item[1]) == True]) * 2 for ruleClass in self.ruleClasses];
-            scores2 = [len([1 for item in question.questionParts if self.containMatch1(ruleClass.qxnWords, item[1]) == True]) * 1 for ruleClass in self.ruleClasses];
-            scores3 = [len([1 for item in question.questionParts if self.containMatch2(ruleClass.qxnWords, item[1]) == True]) * 1 for ruleClass in self.ruleClasses];
-            
-            scores4 = [len([1 for item in question.questionParts if self.exactMatch(ruleClass.keywords, item[1]) == True]) * 4 for ruleClass in self.ruleClasses];
-            scores5 = [len([1 for item in question.questionParts if self.containMatch1(ruleClass.keywords, item[1]) == True]) * 2 for ruleClass in self.ruleClasses];
-            scores6 = [len([1 for item in question.questionParts if self.containMatch2(ruleClass.keywords, item[1]) == True]) * 2 for ruleClass in self.ruleClasses];
+            scores1 = [sum([ruleClass.getQxnWordScore(item[1], 'e') for item in question.questionParts]) * 2 for ruleClass in self.ruleClasses];
+            scores2 = [sum([ruleClass.getQxnWordScore(item[1], 'c1') for item in question.questionParts]) * 1 for ruleClass in self.ruleClasses];
+            scores3 = [sum([ruleClass.getQxnWordScore(item[1], 'c2') for item in question.questionParts]) * 1 for ruleClass in self.ruleClasses];
+
+            scores4 = [sum([ruleClass.getKeywordScore(item[1], 'e') for item in question.questionParts]) * 4 for ruleClass in self.ruleClasses];
+            scores5 = [sum([ruleClass.getKeywordScore(item[1], 'c1') for item in question.questionParts]) * 2 for ruleClass in self.ruleClasses];
+            scores6 = [sum([ruleClass.getKeywordScore(item[1], 'c2') for item in question.questionParts]) * 2 for ruleClass in self.ruleClasses];
 
             totalScores = [scores1[i] + scores2[i] + scores3[i] + scores4[i] + scores5[i] + scores6[i] for i in range(len(scores1))];
 
@@ -221,7 +255,7 @@ class RuleBasedQuestionClassification:
 
             self.finalCategory.append(self.ruleClasses[indexOfBestScore]);
             
-            print(self.ruleClasses[indexOfBestScore].coarseCategory + '\t' + self.ruleClasses[indexOfBestScore].fineCategory);
+            print(self.ruleClasses[indexOfBestScore].coarseCategory + '\t' + self.ruleClasses[indexOfBestScore].fineCategory+ '\t'+ str(max(totalScores)));
         
 analyzer = QuestionAnalysis(Parser().parseFile(qFilePath, qParsedFilePath))
 rule = RuleBasedQuestionClassification(analyzer.questions, classPath, classQuestionWordsPath, classQuestionKeywordsPath);
